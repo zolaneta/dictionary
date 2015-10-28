f60da2d6f65f14007f3a03cbd1ae138993b98a82
zolaneta <zolaneta@yahoo.com>
2015-10-28 16:54:55 -0400

    I added unfinished script

---
+++ dic4ed.py
@@ -0,0 +1,17 @@
+# The Best Dictionary in The World
+# created in Sleepy Hollow NY  on October 28, 2015
+# by Aneta Zolkiewicz for Edward Eames
+
+
+print "You are the best in the world!"
+
+world_best_dic = {'You are': "Jestes", 'the best': "najlepsza", 'in': "na", 'the world': "swiecie"}
+
+
+print world_best_dic['You are'], world_best_dic['the best'], world_best_dic['in'], world_best_dic['the world'] + "!"
+
+print " "
+print "Words to learn: "
+ 
+for word in world_best_dic:
+    print word, " - ", world_best_dic[word]
