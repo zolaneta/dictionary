16ae8526d4dd47985074c38945c6469c57e0ef1c
zolaneta <zolaneta@yahoo.com>
2015-10-29 12:16:47 -0400

    I added user input

---
+++ dic4ed_2.py
@@ -0,0 +1,30 @@
+# The Best Dictionary in The World
+# created in Sleepy Hollow NY  on October 28, 2015
+# by Aneta Zolkiewicz for Edward Eames
+
+
+print "You are the best in the world!"
+
+world_best_dic = {'you are': "jestes", 'the best': "najlepsza", 'in': "na", 'the world': "swiecie"}
+
+
+print world_best_dic['you are'], world_best_dic['the best'], world_best_dic['in'], world_best_dic['the world'] + "!"
+
+print " "
+print "Words to learn: "
+ 
+for word in world_best_dic:
+    print word, " - ", world_best_dic[word]
+
+print " "
+
+word_searched = raw_input("Edziu what word are you looking for? ")
+for word in world_best_dic:
+    if word_searched in world_best_dic:
+        word = word_searched
+        
+        print word, " - ", world_best_dic[word]
+        break
+    else:
+        print " Edziu the dictionary doesn't contain that word!"
+        break
