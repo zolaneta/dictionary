774855feb6ff11164bbc15d6177d2ec15f139d95
zolaneta <zolaneta@yahoo.com>
2015-11-02 10:57:18 -0500

    added \n

--- dic4ed.py
+++ dic4ed.py
@@ -3,15 +3,14 @@
 # by Aneta Zolkiewicz for Edward Eames
 
 
-print "You are the best in the world!"
+print "You are the best in the world!\n"
 
 world_best_dic = {'You are': "Jestes", 'the best': "najlepsza", 'in': "na", 'the world': "swiecie"}
 
 
-print world_best_dic['You are'], world_best_dic['the best'], world_best_dic['in'], world_best_dic['the world'] + "!"
+print world_best_dic['You are'], world_best_dic['the best'], world_best_dic['in'], world_best_dic['the world'] + "!\n"
 
-print " "
-print "Words to learn: "
+print "Words to learn:\n "
  
 for word in world_best_dic:
     print word, " - ", world_best_dic[word]
